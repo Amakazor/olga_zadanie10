14e576f72b2744854edaebbd5d7d9686423830a7
amakazor <amakazor@gmail.com>
2020-02-18 22:40:42 +0100

    init

---
+++ olga_zadanie10/olga_zadanie10.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+/*ZADANIE 10*/
+/*Temat: Referencje*/
+
+int main()
+{
+	/*ZADANIE 10.1*/
+	//Utwórz funkcję typu bool o nazwie sort_5()
+	//1. Funkcja przyjmuje 5 argumentów
+	//2. Argumentami są liczby całkowite i/lub referencje do liczb całkowitych
+	//3. Na potrzeby tej funkcji napisz kolejną funkcję, zamieniającą dwie liczby całkowite wartościami, podobną do tej z przykładów do zadania 9. Ale nie podglądaj w nim!
+	//4. Funkcja przetwarza zamienia argumenty wartościami tak długo aż ich wartości będą w kolejności rosnącej, to jest: argument pierwszy miał najnijższą wartość, a argument piąty najwyższą
+	//5. Sortowanie następuje, jeśli w podanych pięciu argumentach nie ma powtarzających się wartości. Jeśli takie są, to funkcja nie sortuje i zwraca false
+	//6. Jeśli sortowanie nastąpiło to funkcja zwraca true
+	//7. Przetestuj funkcję dla następujących zestawów liczb:
+	//	 10 7 8 4 2
+	//   167 850 34 -15 0
+	//   758 57 8 -56 57
+	//7.1. Wypisz liczby przed sortowaniem
+	//7.2. Sprawdź czy nastąpiło sortowanie
+	//7.3. Jeśli tak to wypisz posortowane liczby, jeśli nie to odpowiedni komunikat
+}
